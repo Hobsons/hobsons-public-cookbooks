f45e2f1f0bd7fa2118ced4a9375fa2e7be6858b0
Patrick McFadin <pmcfadin@gmail.com>
2011-12-19 23:14:30 -0800

    New cookbooks added

---
+++ .chef/knife.rb
@@ -0,0 +1,6 @@
+current_dir = File.dirname(__FILE__)
+cookbook_path ["#{current_dir}/../cookbooks"]
+
+cookbook_copyright "Hobsons"
+cookbook_email     "pmcfadin@hobsons.com"
+cookbook_license   "apachev2"
