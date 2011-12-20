0122ebbd18b4792269c2eb965a65d2ab4951e490
Patrick McFadin <pmcfadin@gmail.com>
2011-12-20 15:15:38 -0800

    Updated stated package to work with RightScale

--- cookbooks/statsd/metadata.rb
+++ cookbooks/statsd/metadata.rb
@@ -8,3 +8,23 @@ version          "0.0.1"
 depends "build-essential"
 depends "git"
 supports "ubuntu"
+
+recipe  "statsd::default", "Installs statsd for use with graphite"
+
+attribute "statsd/port",
+  :display_name => "Statsd port",
+  :description => "Listening port for statsd. Destination for sending events.",
+  :required => true,
+  :recipes => [ "statsd::default"]
+  
+attribute "statsd/graphite_port",
+  :display_name => "Graphite port",
+  :description => "Listening port for graphite. Statsd will forward events to this port",
+  :required => true,
+  :recipes => [ "statsd::default"]
+  
+attribute "statsd/graphite_host",
+  :display_name => "Graphite host",
+  :description => "Host address for graphite. Statsd will forward events to this host.",
+  :required => true,
+  :recipes => [ "statsd::default"]
