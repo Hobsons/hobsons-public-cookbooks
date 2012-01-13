b2b062ca490eb1e72eab8419298c15db49fe0c3c
Patrick McFadin <pmcfadin@gmail.com>
2012-01-12 16:12:16 -0800

    Updated dependencies

--- cookbooks/s3fs/metadata.rb
+++ cookbooks/s3fs/metadata.rb
@@ -4,3 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures s3fs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+recipe           "s3fs", "Installs s3fs from source"
+
+depends          "build-essential"
