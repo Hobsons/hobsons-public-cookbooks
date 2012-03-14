04a967c4eb3e5e96b95f79d9dac370b1409311f0
Patrick McFadin <pmcfadin@gmail.com>
2012-03-13 18:57:15 -0700

    Checkin

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,3 +4,8 @@ cookbook_path ["#{current_dir}/../cookbooks"]
 cookbook_copyright "Hobsons"
 cookbook_email     "pmcfadin@hobsons.com"
 cookbook_license   "apachev2"
+
+# EC2:
+knife[:aws_access_key_id]     = "AKIAIH5V4RWJOLOXNTBA"
+knife[:aws_secret_access_key] = "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"
+
